51a830778c487590ea4cbaa548a860ba596a3264
paulkjdb <118433690+paulkjdb@users.noreply.github.com>
2022-11-22 11:39:47 +0300

    Task 1 Done

--- 00_distance.py
+++ 00_distance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
+from pprint import pprint
 
 sites = {
     'Moscow': (550, 370),
@@ -12,12 +13,26 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = dict()
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites ['Paris']
 
-print(distances)
 
+moscow_london = ((moscow[0] - london[0]) **2 + (moscow[1] - london[1]) **2 ) ** .5
+moscow_paris = ((moscow[0] - paris[0]) **2 + (moscow[1] - paris[1]) **2 ) ** .5
+london_paris = ((london[0] - paris[0]) **2 + (london[1] - paris[1])**2)   ** .5
+
+distances ['Moscow'] = {}
+distances ['Moscow']['London'] = moscow_london
+distances ['Moscow']['Paris'] = moscow_paris
+
+distances ['London'] = {}
+distances ['London']['Paris'] = london_paris
+distances ['London']['Moscow'] = moscow_london
+
+pprint(distances)
 
 
 
