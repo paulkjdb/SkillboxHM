b077592a0b112455b07e6944d55cac0a4fed46a9
paulkjdb <118433690+paulkjdb@users.noreply.github.com>
2022-11-22 20:04:11 +0300

    Task 10 Done!!!

--- 10_store.py
+++ 10_store.py
@@ -43,6 +43,38 @@ lamps_price = lamps_item['price']
 lamps_cost = lamps_quantity * lamps_price
 print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
 
+table_cost = store[goods['Стол']][1]['quantity'] * store[goods['Стол']][1]['price']
+# или проще (/сложнее ?)
+table_code = goods['Стол']
+table_item1 = store[table_code][0]
+table_item2 = store[table_code][1]
+table_quantity1 = table_item1['quantity']
+table_quantity2 = table_item2['quantity']
+table_quantity = table_quantity1 + table_quantity2
+table_price1 = table_item1['price']
+table_price2 = table_item2['price']
+table_cost = table_quantity1 * table_price1 + table_quantity2 * table_price2
+print('Стол -', table_quantity, 'шт, стоимость', table_cost, 'руб')
+
+#table_cost = store[goods['Стол']][1]['quantity'] * store[goods['Стол']][1]['price']
+# или проще (/сложнее ?)
+coach_code = goods['Диван']
+coach_item1 = store[coach_code][0]
+coach_item2 = store[coach_code][1]
+coach_quantity1 = coach_item1['quantity']
+coach_quantity2 = coach_item2['quantity']
+coach_quantity = coach_quantity1 + coach_quantity2
+coach_price1 = coach_item1['price']
+coach_price2 = coach_item2['price']
+coach_cost = coach_quantity1 * coach_price1 + coach_quantity2 * coach_price2
+print('Диван -', coach_quantity, 'шт, стоимость', coach_cost, 'руб')
+
+
+
+
+
+
+
 # Вывести стоимость каждого товара на складе: один раз распечать сколько всего столов, стульев и т.д. на складе
 # Формат строки <товар> - <кол-во> шт, стоимость <общая стоимость> руб
 
@@ -50,6 +82,24 @@ print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, '�
 # Это задание на ручное вычисление - что бы потом понять как работают циклы и насколько с ними проще жить.
 
 # TODO здесь ваш код
+
+chair_code = goods['Стул']
+chair_item1 = store[chair_code][0]
+chair_item2 = store[chair_code][1]
+chair_item3 = store[chair_code][2]
+chair_quantity1 = chair_item1['quantity']
+chair_quantity2 = chair_item2['quantity']
+chair_quantity3 = chair_item3['quantity']
+chair_quantity = chair_quantity1 + chair_quantity2 + chair_quantity3
+chair_price1 = chair_item1['price']
+chair_price2 = chair_item2['price']
+chair_price3 = chair_item3['price']
+chair_cost = chair_quantity1 * chair_price1 + chair_quantity2 * chair_price2 + chair_quantity3 * chair_price3
+print('Стул -', chair_quantity, 'шт, стоимость', chair_cost, 'руб')
+
+
+
+
 
 ##########################################################################################
 # ВНИМАНИЕ! После того как __ВСЯ__ домашняя работа сделана и запушена на сервер,         #
